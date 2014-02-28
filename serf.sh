26e561430d6dc37d17dccd2d56b460cd523ece2a
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-02-28 17:03:55 +0000

    added member join and member leave scripts

--- serf.sh
+++ serf.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-exec serf agent -role $SERF_ROLE >> /var/log/serf.log 2>&1
+exec serf agent -role $SERF_ROLE \
+    -event-handler="member-join=/member_join.sh" \
+    -event-handler="member-leave,member-failed=/member_leave.sh" >> /var/log/serf.log 2>&1
