f64428ec812849a635ada24b1075b016e3e9490d
Fareed Dudhia <fareeddudhia@googlemail.com>
2014-03-01 20:50:40 +0000

    added serf script

--- serf.sh
+++ serf.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 exec serf agent -role $SERF_ROLE \
-    -event-handler="member-join=/member_join.sh" \
-    -event-handler="member-leave,member-failed=/member_leave.sh" >> /var/log/serf.log 2>&1
+    -event-handler="member-join=python member_join.py" \
+    -event-handler="member-leave,member-failed=python member_leave.py" >> /var/log/serf.log 2>&1
